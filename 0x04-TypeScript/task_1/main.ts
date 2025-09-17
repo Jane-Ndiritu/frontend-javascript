6aaf25abd3b1b1863b620fc4cf53f4d6b3621095
jane <jayndiritu36@gmail.com>
2025-09-17 15:47:04 +0300

    Creating functions specific to employees

--- 0x04-TypeScript/task_1/main.ts
+++ 0x04-TypeScript/task_1/main.ts
@@ -16,3 +16,48 @@ const teacher: Teacher  = {
   contract: false
 };
 console.log(teacher);
+type Directors = {
+  firstName: string;
+  lastName: string;
+  location: string;
+  fullTimeEmployee: boolean;
+  numberOfReports: number;
+};
+
+const director: Directors = {
+  firstName: 'John',
+  lastName: 'Mathew',
+  location: 'Nairobi',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director);
+type printTeacherFunction = (firstName: string, lastName: string) => string;
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  const firstInitial = firstName.charAt(0).toUpperCase();
+  return `${firstInitial}. ${lastName}`;
+};
+console.log(printTeacher("samuel", "kamau"));
+console.log(printTeacher("john", "mathew"));
+
+interface StudentClass {
+ new (firstName: string, lastName: string): StudentClass;
+  workOnHomework(): string;
+  displayName(): string;
+}
+class StudentClass implements StudentClass{
+    constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+    displayName(): string {
+    return this.firstName;
+    }
+}
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
---
+++ 0x04-TypeScript/task_2/main.js
@@ -0,0 +1,9 @@
+var teacher = {
+    firstName: "samuel",
+    lastName: "kamau",
+    fulltimeEmployee: false,
+    yearsOfExperience: 5,
+    location: "Nairobi",
+    contract: false
+};
+console.log(teacher);
