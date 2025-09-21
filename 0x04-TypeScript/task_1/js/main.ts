593c7c353a06eb6aa6a2ebc0b57476d8b7d4ee0b
jane <jayndiritu36@gmail.com>
2025-09-21 19:18:59 +0300

    updated teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,8 @@
-function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName.charAt(0).toUpperCase()}. ${lastName}`;
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
 }
-console.log(printTeacher("john", "doe"));
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+console.log(printTeacher("John", "Doe")); // Output: J. Doe
+console.log(printTeacher("Jane", "Smith")); // Output: J. Smith
