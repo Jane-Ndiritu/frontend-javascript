123bcb664069450583a664cdf0e4598607d7ae72
jane <jayndiritu36@gmail.com>
2025-09-21 18:02:19 +0300

    Printing teachers

--- 0x04-TypeScript/task_1/main.ts
+++ 0x04-TypeScript/task_1/main.ts
@@ -1,10 +1,9 @@
 interface printTeacherFunction {
-  (teacher: { firstName: string; lastName: string }): string;
+  (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = ({ firstName, lastName }) => {
-  return `${firstName.charAt(0).toUpperCase()}. ${lastName}`;
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName}. ${lastName}`;
 };
 
-console.log(printTeacher({ firstName: "samuel", lastName: "kamau" })); // S. kamau
-console.log(printTeacher({ firstName: "john", lastName: "mathew" })); // J. mathew
+console.log(printTeacher("John", "Doe"));
